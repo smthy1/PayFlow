8b4807f7b76906f7584c07551fd74716e75d7bc9
smthy1 <luiz.smith.br@gmail.com>
2025-11-11 22:56:24 -0300

    atualiza conexão do prisma client

--- src/modules/prisma/prisma.services.ts
+++ src/modules/prisma/prisma.services.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import 'dotenv/config'
 
 
-const prisma =  new PrismaClient();
+const prisma =  new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL as string,
+        }
+    },
+});
 
 
 export default prisma;
