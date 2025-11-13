f80f530645579460bb81898809af91a1506272da
smthy1 <luiz.smith.br@gmail.com>
2025-11-13 20:01:19 -0300

    fix

--- src/modules/prisma/prisma.services.ts
+++ src/modules/prisma/prisma.services.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma =  new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL as string,
-        }
-    },
-});
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
