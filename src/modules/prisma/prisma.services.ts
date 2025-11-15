9de13f86731e454dd330420edaafc289e376da40
smthy1 <luiz.smith.br@gmail.com>
2025-11-15 08:47:36 -0300

    teste de conexão

--- src/modules/prisma/prisma.services.ts
+++ src/modules/prisma/prisma.services.ts
@@ -3,4 +3,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+prisma.$connect().catch((err)=> {
+    console.error('Prisma connection failed: ', err);
+    process.exit(1);
+});
+
 export default prisma;
