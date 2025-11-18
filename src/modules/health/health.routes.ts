d7e3a0d5e48d48cfdfb931b957e0ad06bba719fb
smthy1 <luiz.smith.br@gmail.com>
2025-11-17 23:13:21 -0300

    remove rota alternativa de health check

--- src/modules/health/health.routes.ts
+++ src/modules/health/health.routes.ts
@@ -1,6 +1,5 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { authToken } from "../../middlewares/authMiddleware.js";
-import { type HealthCheck, healthCheckSchema, healthCheckResponseSchema  } from "./health.schemas.js";
+import { healthCheckResponseSchema  } from "./health.schemas.js";
 
 
 const  healthRoutes = async (app: FastifyInstance) => {
@@ -15,28 +14,6 @@ const  healthRoutes = async (app: FastifyInstance) => {
         async (req: FastifyRequest, reply: FastifyReply) => {
             try {
                 return reply.status(200).send({ status: "ok" });
-            } catch (err) {
-                return reply.status(500).send({ error: err });
-            }
-        }
-    );
-    
-    app.post<{ Body: HealthCheck }>(
-        '/test', 
-        {
-            preHandler: [authToken],
-            schema: {
-                description: "Health check with login",
-                tags: ['auth'],
-                body: healthCheckSchema,
-                response: { 201: healthCheckSchema }
-            }
-        }, 
-        async (req: FastifyRequest<{ Body: HealthCheck }>, reply) => {
-            try {
-                const { content } = req.body;
-
-                return reply.status(200).send({ content: `Hello ${content}` });
             } catch (err) {
                 return reply.status(500).send({ error: err });
             }
--- src/modules/health/health.schemas.ts
+++ src/modules/health/health.schemas.ts
@@ -1,11 +1,6 @@
 import { z } from 'zod';
 
-export const healthCheckSchema = z.object({
-    content: z.string()
-});
 
 export const healthCheckResponseSchema = z.object({
     status: z.string()
 });
-
-export type HealthCheck = z.infer<typeof healthCheckSchema>;
